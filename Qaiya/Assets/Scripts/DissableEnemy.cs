23f15c6bc6ebe01fccca7d36e9957261288de36c
WTuneSeek <91195791+WTuneSeek@users.noreply.github.com>
2023-01-31 11:48:26 +0100

    Enemy and DissableCheck

---
+++ Qaiya/Assets/Scripts/DissableEnemy.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DissableEnemy : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Trigger");
+    }
+}
