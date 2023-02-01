1fbe1c8cc17339b8cf31f74d6a5422db6aad721e
WTuneSeek <91195791+WTuneSeek@users.noreply.github.com>
2023-02-01 11:23:03 +0100

    FOV

---
+++ Qaiya/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float radius = 5f;
+    [Range(1, 360)]
+    public float angle = 45f;
+
+    public GameObject playerRef;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public bool CanSeePlayer { get; private set; }
+
+    private void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+        
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider2D[] rangeCheck = Physics2D.OverlapCircleAll(transform.position, radius, targetMask);
+
+        if (rangeCheck.Length > 0)
+        {
+            Transform target = rangeCheck[0].transform;
+            Vector2 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector2.Angle(transform.up, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+                if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                {
+                    CanSeePlayer = true;
+                }
+                else
+                {
+                    CanSeePlayer = false;
+                }
+            }
+            else
+            {
+                CanSeePlayer = false;
+            }
+        }
+        else if(CanSeePlayer)
+        {
+            CanSeePlayer = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.forward, radius);
+
+        Vector3 angles01 = DirectionalFromAngle(-transform.eulerAngles.z, -angle / 2);
+        Vector3 angles02 = DirectionalFromAngle(-transform.eulerAngles.z, angle / 2);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + angles01 * radius);
+        Gizmos.DrawLine(transform.position, transform.position + angles02 * radius);
+
+        if (CanSeePlayer)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, playerRef.transform.position);
+        }
+    }
+
+    private Vector2 DirectionalFromAngle(float eulerY, float angleInDegrees)
+    {
+        angleInDegrees += eulerY;
+        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
