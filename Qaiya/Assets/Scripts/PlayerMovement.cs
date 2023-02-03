6a62e5217c205ae1cb6115903dfb3e10b6802ff8
PS2Majima <vdoornhe0403@student.landstede.nl>
2023-02-03 09:58:52 +0100

    Sprinting added

--- Qaiya/Assets/Scripts/PlayerMovement.cs
+++ Qaiya/Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,75 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
+
+    public bool isSprinting;
+    public bool sprintable = true;
+    public float sprintMultiplier;
+    public float sprintTime;
+    public float maxSprintTime = 5f;
     
+    private float totalSpeed;
+    public float countUpTime;
+
+    private void Start()
+    {
+        sprintTime = maxSprintTime;
+        countUpTime = maxSprintTime;
+    }
 
     private void Update()
     {
+        Sprint();
         ProcessInputs();
         
     }
 
-    private void FixedUpdate()
+    private void Sprint()
     {
-        // Move();
+        if (countUpTime < 0)
+        {
+            sprintable = true;
+            countUpTime = maxSprintTime;
+            sprintTime = maxSprintTime;
+        }
+        if (isSprinting && sprintTime > 0 && sprintable == true)
+        {
+            sprintTime -= Time.deltaTime;
+            totalSpeed = moveSpeed * sprintMultiplier;
+        }
+        else
+        {
+            totalSpeed = moveSpeed;
+        }
+
+        if (sprintTime < 0)
+        {
+            sprintable = false;
+            countUpTime -= Time.deltaTime;
+        }
         
     }
 
     private void ProcessInputs()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
+        
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        transform.Translate(Vector3.right * moveX * moveSpeed * Time.deltaTime, Space.World);
-        transform.Translate(Vector3.up * moveY * moveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.right * moveX * totalSpeed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.up * moveY * totalSpeed * Time.deltaTime, Space.World);
 
     }
     
